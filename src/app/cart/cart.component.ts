937296f5e40dd54affe6fb2361c2a256161b8c53
Ravindra Devrani <ravindra.devrani003@gmail.com>
2023-10-10 21:28:56 +0530

    feat(cart) : cart summary ui implemented

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,19 +1,54 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
-import { NgIf, NgFor, AsyncPipe, JsonPipe } from "@angular/common";
-import { CartUiComponent } from "./ui/cart-ui.component";
+import { CartItemComponent } from "./ui/cart-item.component";
+import { Book } from "src/app/book/data/book.model";
+import { CartSummaryComponent } from "./ui/cart-summary.component";
 
 @Component({
   selector: "app-cart",
   standalone: true,
-  imports: [NgIf, NgFor, AsyncPipe, JsonPipe, CartUiComponent],
-  template: ` <app-cart-ui /> `,
+  imports: [CartItemComponent, CartSummaryComponent],
+  template: `
+    <div class="cart-container">
+      <div class="cart-items">
+        <app-cart-item
+          [book]="book"
+          (selectQuantity)="onSelectQuantity($event)"
+        />
+      </div>
+      <app-cart-summary />
+    </div>
+  `,
   styles: [
     `
-      :host {
-        padding: 15px 20px;
+      .cart-container {
+        display: flex;
+        gap: 20px;
+        padding: 15px;
+      }
+
+      .cart-items {
+        width: 62%;
       }
     `,
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CartComponent {}
+export class CartComponent {
+  //TODO: remove after UI testing
+  book: Book = {
+    id: "8ef9f86b-cdc4-49ab-88de-f641e8d0ab73",
+    Author: "Chinua Achebe",
+    Country: "Nigeria",
+    ImageLink: "assets/images/things-fall-apart.jpg",
+    Language: "English",
+    Link: "https://en.wikipedia.org/wiki/Things_Fall_Apart\n",
+    Pages: 209,
+    Title: "Things Fall Apart",
+    Year: 1958,
+    Price: 243,
+  };
+
+  onSelectQuantity(quantity: number) {
+    //TODO: add quantity to store
+  }
+}
--- src/app/cart/ui/cart-summary.component.ts
+++ src/app/cart/ui/cart-summary.component.ts
@@ -1,19 +1,59 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-cart-summary',
+  selector: "app-cart-summary",
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      cart-summary works!
-    </p>
+    <div class="summary-item">
+      <div class="summary-key">SubTotal</div>
+      <div class="summary-value">₹857.00</div>
+    </div>
+
+    <div class="summary-item">
+      <div class="summary-key">Tax</div>
+      <div class="summary-value">₹79.27</div>
+    </div>
+
+    <div class="summary-item">
+      <div class="summary-key bolder">Total</div>
+      <div class="summary-value bolder">₹936.27</div>
+    </div>
   `,
   styles: [
-  ],
-  changeDetection: ChangeDetectionStrategy.OnPush
-})
-export class CartSummaryComponent {
+    `
+      :host {
+        flex-grow: 1;
+        background-color: #f9f9f9;
+        height: 300px;
+        padding: 20px;
+        border-radius: 10px;
+        border: 1px solid rgb(78, 78, 78);
+        box-shadow: 0 4px 6px #0000001a;
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+      }
 
-}
+      .summary-item {
+        display: flex;
+        gap: 10px;
+      }
+
+      .bolder {
+        font-size: 30px !important;
+      }
+
+      .summary-key {
+        font-size: 22px;
+        font-weight: 700;
+      }
+      .summary-value {
+        font-size: 20px;
+      }
+    `,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CartSummaryComponent {}
