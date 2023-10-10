312523cc492f474a64cd239eb54a82199c8c7ad7
Ravindra Devrani <ravindra.devrani003@gmail.com>
2023-10-10 18:34:39 +0530

    feat(header): cart icon

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -11,6 +11,7 @@ import { MatButtonModule } from "@angular/material/button";
 import { RouterModule } from "@angular/router";
 import { NgIf } from "@angular/common";
 import { User } from "../auth/data/user.model";
+import { MatBadgeModule } from "@angular/material/badge";
 
 @Component({
   selector: "app-header",
@@ -20,6 +21,7 @@ import { User } from "../auth/data/user.model";
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
+    MatBadgeModule,
     NgIf,
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -46,6 +48,17 @@ import { User } from "../auth/data/user.model";
           >
             Manage-Books
           </button>
+
+          <button mat-icon-button color="secondary" routerLink="/cart">
+            <mat-icon
+              [matBadge]="1"
+              matBadgePosition="above after"
+              matBadgeColor="accent"
+            >
+              shopping_cart
+            </mat-icon>
+          </button>
+
           <button mat-button (click)="logout.emit()" routerLinkActive="active">
             Logout
           </button>
@@ -64,12 +77,22 @@ import { User } from "../auth/data/user.model";
         </ng-container>
 
         <a
-          href=""
+          href="https://github.com/rd003/BookFullStack"
+          target="_blank"
           mat-icon-button
           class="example-icon favorite-icon"
           aria-label="Example icon-button with heart icon"
         >
-          <mat-icon>favorite</mat-icon>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+          >
+            <path
+              d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
+            />
+          </svg>
         </a>
       </mat-toolbar>
     </p>
