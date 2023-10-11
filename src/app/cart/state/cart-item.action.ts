e631a90af1ae172cd8298a190de934166a29916a
Ravindra Devrani <ravindra.devrani003@gmail.com>
2023-10-11 17:20:59 +0530

    feat(cart): cart item actions added

---
+++ src/app/cart/state/cart-item.action.ts
@@ -0,0 +1,21 @@
+import { createActionGroup, emptyProps, props } from "@ngrx/store";
+import { CartItem } from "../cart.model";
+import { HttpErrorResponse } from "@angular/common/http";
+
+export const CartItemActions = createActionGroup({
+  source: "CartItem",
+  events: {
+    "Load Cart Items": emptyProps(),
+    "Load Cart Item Success": props<{ cartItems: CartItem[] }>(),
+    "Load Cart Item Failure": props<{ error: HttpErrorResponse }>(),
+    "Add Cart Item": props<{ cartItem: CartItem }>(),
+    "Add Cart Item Success": props<{ cartItem: CartItem }>(),
+    "Add Cart Item Failure": props<{ error: HttpErrorResponse }>(),
+    "Update Cart Item": props<{ cartItem: CartItem }>(),
+    "Update Cart Item Success": props<{ cartItem: CartItem }>(),
+    "Update Cart Item Failure": props<{ error: HttpErrorResponse }>(),
+    "Remove Cart Item": props<{ id: string }>(),
+    "Remove Cart Item Success": props<{ id: string }>(),
+    "Remove Cart Item Failure": props<{ error: HttpErrorResponse }>(),
+  },
+});
