b7aba90a301abacde9073046dce461931d7b9cc2
Ravindra Devrani <ravindra.devrani003@gmail.com>
2023-10-12 13:04:40 +0530

    patch: cart item action

--- src/app/cart/state/cart-item.action.ts
+++ src/app/cart/state/cart-item.action.ts
@@ -5,7 +5,7 @@ import { HttpErrorResponse } from "@angular/common/http";
 export const CartItemActions = createActionGroup({
   source: "CartItem",
   events: {
-    "Load Cart Items": emptyProps(),
+    "Load Cart Items": props<{ cartId: string }>(),
     "Load Cart Item Success": props<{ cartItems: CartItem[] }>(),
     "Load Cart Item Failure": props<{ error: HttpErrorResponse }>(),
     "Add Cart Item": props<{ cartItem: CartItem }>(),
